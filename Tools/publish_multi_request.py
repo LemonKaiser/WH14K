1d173a37aceb0028a5f047740b5bb277c01c6d08
OCO_Omega <42233446+OCOtheOmega@users.noreply.github.com>
2026-01-29 14:28:15 +0300

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,8 +15,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.simplestation.org/"
-FORK_ID = "ebengrad"
+ROBUST_CDN_URL = "https://2612.koara.live/"
+FORK_ID = "main"
 
 def main():
     parser = argparse.ArgumentParser()
