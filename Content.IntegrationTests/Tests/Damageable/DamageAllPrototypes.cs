4d04c5f56b0f59701abaef43c4646fae5d2939e4
OCO_Omega <42233446+OCOtheOmega@users.noreply.github.com>
2026-05-04 17:04:13 +0300

    textures update

--- Content.IntegrationTests/Tests/Damageable/DamageAllPrototypes.cs
+++ Content.IntegrationTests/Tests/Damageable/DamageAllPrototypes.cs
@@ -1,5 +1,5 @@
+using System.Linq;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using Content.IntegrationTests.Fixtures;
 using Content.IntegrationTests.Fixtures.Attributes;
 using Content.IntegrationTests.Utility;
@@ -19,42 +19,53 @@ public sealed class DamageAllPrototypesTest : GameTest
 {
     [SidedDependency(Side.Server)] private readonly DamageableSystem _damageableSystem = default!;
 
-    private static string[] _damageables = GameDataScrounger.EntitiesWithComponent("Damageable");
+    private static readonly string[] Damageables = GameDataScrounger.EntitiesWithComponent("Damageable");
 
     [Test]
     [TestOf(typeof(DamageableSystem))]
-    [TestCaseSource(nameof(_damageables))]
     [Description("Ensures all Entity Prototypes with damageable can be damaged.")]
-    public async Task TestDamageableComponents(string damageable)
+    public async Task TestDamageableComponents()
     {
         var map = await Pair.CreateTestMap();
 
-        var entity = await SpawnAtPosition(damageable, map.GridCoords);
-
-        // Intentionally cannot take damage, ignore it.
-        if (SEntMan.HasComponent<GodmodeComponent>(entity))
-            return;
-
-        var canBeDamaged = false;
-
-        foreach (var type in SProtoMan.EnumeratePrototypes<DamageTypePrototype>())
+        await Server.WaitAssertion(() =>
         {
-            if (!_damageableSystem.CanBeDamagedBy(entity, type))
-                continue;
+            var damageTypes = SProtoMan.EnumeratePrototypes<DamageTypePrototype>().ToArray();
 
-            canBeDamaged = true;
-
-            await Server.WaitPost(() =>
+            using (Assert.EnterMultipleScope())
             {
-                var damage = new DamageSpecifier(type, FixedPoint2.Epsilon);
-                var previousDamage = _damageableSystem.GetTotalDamage(entity);
-                _damageableSystem.ChangeDamage(entity, damage, ignoreResistances: true);
-                Assert.That(_damageableSystem.GetTotalDamage(entity) == FixedPoint2.Epsilon + previousDamage);
-                _damageableSystem.ClearAllDamage(entity);
-            });
-        }
+                for (var i = 0; i < Damageables.Length; i++)
+                {
+                    var damageable = Damageables[i];
+                    var coords = map.GridCoords.Offset(new Vector2(i % 32, i / 32));
+                    var entity = SSpawnAtPosition(damageable, coords);
 
-        // Ensure that this entity can actually be damaged.
-        Assert.That(canBeDamaged);
+                    try
+                    {
+                        // Intentionally cannot take damage, ignore it.
+                        if (SEntMan.HasComponent<GodmodeComponent>(entity))
+                            continue;
+
+                        // This test only needs to prove the prototype can take at least one valid damage type.
+                        var damageType = damageTypes.FirstOrDefault(type => _damageableSystem.CanBeDamagedBy(entity, type));
+
+                        Assert.That(damageType, Is.Not.Null, $"{damageable} has Damageable but cannot be damaged by any damage type.");
+                        if (damageType is null)
+                            continue;
+
+                        var damage = new DamageSpecifier(damageType, FixedPoint2.Epsilon);
+                        var previousDamage = _damageableSystem.GetTotalDamage(entity);
+                        _damageableSystem.ChangeDamage(entity, damage, ignoreResistances: true);
+                        Assert.That(_damageableSystem.GetTotalDamage(entity), Is.EqualTo(FixedPoint2.Epsilon + previousDamage));
+                        _damageableSystem.ClearAllDamage(entity);
+                    }
+                    finally
+                    {
+                        if (!SEntMan.Deleted(entity))
+                            SEntMan.DeleteEntity(entity);
+                    }
+                }
+            }
+        });
     }
 }
---
+++ Resources/Locale/en-US/_wh40k/prototypes/entities/psyker_staves.ftl
@@ -0,0 +1,11 @@
+ent-WeaponPsykerForceStaffBiomantic = biomantic force staff
+    .desc = A psychically-attuned force stave used by sanctioned psykers to channel biomancy, knitting flesh and forcing the body back into obedience through the will of the Warp.
+
+ent-WeaponPsykerForceStaffPyromantic = pyromantic force staff
+    .desc = A force stave shaped for pyromancy. It casts focused bolts of warpfire, the most spectacular and destructive of sanctioned battlefield disciplines.
+
+ent-WeaponPsykerForceStaffPsychic = psychic force staff
+    .desc = A force stave built as a direct conduit for disciplined warp discharge. Imperial psykers use such weapons to hurl compact blasts of mind-driven force at short range.
+
+ent-WeaponPsykerForceStaffTelekinetic = telekinetic force staff
+    .desc = A force stave aligned to telekinetic practice. Its shock-bolt strikes like an invisible hammer, flinging foes with compressed force drawn from the Immaterium.
